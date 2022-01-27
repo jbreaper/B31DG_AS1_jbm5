1620afdf96edef2c0c5bab77116af4842dc4bec5
jbreaper <jbmavor@hotmail.co.uk>
2022-01-27 17:02:43 +0000

    file created
    file created, basic initial structure coded

---
+++ em_as1_jbm5.ino
@@ -0,0 +1,30 @@
+#define int a = 1300;  // m = 13 => 1300 nano-seconds
+#define int b = 100;   // a = 1  => 100  nano-seconds
+#define int c = 9;     // v = 5  => 9
+#define int b = 600;   // o = 12 => 600  nano-seconds
+#define int mode = 2;  // r = 9  => 2
+#define int testing = 1000;
+
+#define SIGA
+#define SIGB
+#define PB1
+#define PB2
+
+switch(cMode){
+  case 1:
+    
+    break;
+  case 2:
+    break;
+  case 3:
+    break;
+  case 4:
+    break;
+  default:
+    
+    break;
+}
+
+void loop(){
+  
+}
