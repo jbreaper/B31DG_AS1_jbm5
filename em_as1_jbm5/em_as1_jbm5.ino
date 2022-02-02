868d1089d52c2c177f7ba2d2ab44079c4f762a41
jbreaper <jbmavor@hotmail.co.uk>
2022-02-02 17:59:32 +0000

    initial NS diagram created

--- em_as1_jbm5/em_as1_jbm5.ino
+++ em_as1_jbm5/em_as1_jbm5.ino
@@ -1,24 +1,48 @@
 // programmed for the "DOIT ESP32 DEVKIT V1" board
+#include <stdio.h>
+#include <stdbool.h>
+#include <Wire.h>
 
-#define int a = 1300;  // m = 13 => 1300 nano-seconds
-#define int b = 100;   // a = 1  => 100  nano-seconds
-#define int c = 9;     // v = 5  => 9
-#define int b = 600;   // o = 12 => 600  nano-seconds
-#define int mode = 2;  // r = 9  => 2
-#define int testing = 1000;
+#define SIGA    15    // Pin G15, Used for the output of Signal A
+#define SIGB    21    // Pin G21, Used for the output of Signal B
+#define PB1     22    // Pin G22
+#define PB2     23
 
-#define SIGA
-#define SIGB
-#define PB1
-#define PB2
+#define A       1300  // m = 13 => 1300 nano-seconds
+#define B       100   // a = 1  => 100  nano-seconds
+#define C       9     // v = 5  => 9
+#define D       6000  // o = 12 => 6000 nano-seconds
+#define MODE    2     // r = 9  => 2
+#define SECONDS 1000
+
+const bool testing = true;
+
+#define TEST (testing*SECONDS)
+
+void setup() {
+  pinMode(SIGA, OUTPUT);
+  pinMode(SIGB, OUTPUT);
+
+  pinMode(PB1, INPUT);
+  pinMode(PB2, INPUT);
+}
+
+void signal(unsigned int pulse_width, unsigned int rest_width, unsigned int pulse_count, unsigned int block_rest, bool direction) {
 
 
+  for (int i = 0; i++; i < pulse_count) {
+    int x = direction * pulse_count;
 
-void loop(){
-  
-  switch(mode){
+    delayMicroseconds((pulse_width + (50 * (abs(x - i))))*TEST);
+  }
+}
+
+
+void loop() {
+
+  switch (MODE) {
     case 1:
-      
+      delayMicroseconds(A);
       break;
     case 2:
       break;
@@ -27,8 +51,8 @@ void loop(){
     case 4:
       break;
     default:
-      
+
       break;
   }
-  
+
 }
